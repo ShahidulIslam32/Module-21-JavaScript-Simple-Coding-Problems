46f9483de0feba4f3693bf0089acbdc248445f89
shahidul islam <islamkhans148@gmail.com>
2022-02-01 12:14:26 +0600

    done

--- largest-number.js
+++ largest-number.js
@@ -16,23 +16,3 @@ let ages = [26,36,46,56,89,78,140,100,20];
 let oldest = largestElement(ages);
 console.log('oldest Member is', oldest)
 
-// smalest number
-
-
-function samllestElement(smallers){
-    let small = 0;
-    for (let i=0; i < smallers.length; i++){
-        let elements = smallers[i]
-        if(elements < small){
-            small = elements
-
-        }
-    }
-    return small
-
-}
-
-let age = [26,36,46,56,89,78,140,100,20];
-let small = largestElement(age);
-console.log('Young Member is', small)
-
