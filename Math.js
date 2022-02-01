29981c5b80561b4215b27ebaa16886c3568e1eaf
shahidul islam <islamkhans148@gmail.com>
2022-02-01 10:49:55 +0600

    2 files done

---
+++ Math.js
@@ -0,0 +1,19 @@
+
+let myNumber = -50;
+let output = Math.abs(myNumber)
+console.log(output)
+
+let yourNumber = 3.3689
+let result = Math.ceil(yourNumber)
+console.log(result)
+
+let thirdNumber = Math.random() * 10
+console.log(thirdNumber)
+
+function setRandomNumber(number){
+    for(let i=0; i <= 20; i++){
+        let numberOne = Math.random() * 23
+        console.log(numberOne)
+    }
+}
+setRandomNumber(20)
---
+++ Swap.js
@@ -0,0 +1,17 @@
+
+let first = 5
+let second = 7
+console.log(first, second)
+
+let temp = first
+first = second
+console.log(temp)
+
+let First = 30;
+let Second = 45;
+console.log(First,Second)
+
+let Temp = First; //30
+First = Second;
+Second = Temp
+console.log(First, Second)
