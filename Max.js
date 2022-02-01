9c8347df8f26cc328846cf77a49094a83ca047c7
shahidul islam <islamkhans148@gmail.com>
2022-02-01 11:18:53 +0600

    max-min numbr find done

---
+++ Max.js
@@ -0,0 +1,16 @@
+// max number find
+function isLarge(one,two,three){
+    let result = Math.max(one,two,three)
+    console.log('large number is',result)
+}
+
+isLarge(1250,350,450)
+
+// min number
+
+function isLower(one,two,three){
+    let result = Math.min(one,two,three)
+    console.log('lower number is',result)
+}
+
+isLower(1250,350,450)
